bf75e0146c25aacee6c21e52d66704c511e74fee
Gustavo Pimentel <pimentel@oktagongames.com>
2020-06-30 17:42:40 -0300

    Inserindo code para funcionamento

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, transaction) => {
+      if (transaction.type === 'income') {
+        return acc + transaction.value;
+      }
+
+      return acc;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acc, transaction) => {
+      if (transaction.type === 'outcome') {
+        return acc + transaction.value;
+      }
+      return acc;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
